20e01147f916fc1434b170d87559c4668c4ce316
ggsno <ogs0518@gmail.com>
2023-04-22 20:31:12 +0900

    feat: 로그아웃 추가

--- src/components/Lobby.tsx
+++ src/components/Lobby.tsx
@@ -2,15 +2,32 @@ import { useRecoilValue } from "recoil";
 import { userState } from "../atoms/user";
 import TeacherLobby from "./Lobby.Teacher";
 import StudentLobby from "./Lobby.Student";
+import { storage } from "../utils/storage";
+import { useNavigate } from "react-router-dom";
+import { Urls } from "../pages/router";
 
 export default function Lobby() {
   const user = useRecoilValue(userState);
   if (!user) throw new Error("no user state");
+  const navigator = useNavigate();
+  const handleLogout = () => {
+    storage.remove("ACCESS_TOKEN");
+    storage.remove("USER_ID");
+    navigator(Urls.login);
+  };
 
   return (
     <div className="bg-gray-light min-h-screen">
       <div className="flex justify-center bg-[#e6edf8] text-violet-blue p-10">
         <header className="max-w-[60rem] grow">
+          <div className="flex justify-end">
+            <button
+              onClick={handleLogout}
+              className="border py-1 px-3 rounded-md mb-5"
+            >
+              logout
+            </button>
+          </div>
           <h1 className="text-4xl font-light">
             Welcome, {user?.name}! Take a practice test and get ready for test
             day.
