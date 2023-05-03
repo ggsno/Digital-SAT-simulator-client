c8a6be805313b8422de00738d0e3e682f2c84c34
ggsno <ogs0518@gmail.com>
2023-05-03 20:10:47 +0900

    test: msw 제거, 테스트서버 연결

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,9 @@ import "./styles/global.css";
 import { RecoilRoot } from "recoil";
 import { RouterProvider } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import { worker } from "../mocks/browser";
 import { router } from "./pages/router";
 import { QueryClient, QueryClientProvider } from "react-query";
 import Loading from "./components/Loading";
-
-if (process.env.NODE_ENV === "development") {
-  worker.start();
-}
 
 export const queryClient = new QueryClient();
 
